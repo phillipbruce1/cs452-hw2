59dafc1c02cd45e88129891786afbc9031a2c28d
Phillip <phillip.bruce@hey.com>
2021-10-08 17:34:16 -0600

    Expanding code makes me cringe

--- hw2/main.c
+++ hw2/main.c
@@ -25,7 +25,8 @@ static void consume(void *a) {
     void **args = a;
     Deq q = (Deq) args[0];
     Lawn l = (Lawn) args[1];
-    mole_whack(*mtq_head_get(q));
+    Data d = mtq_head_get(q);
+    mole_whack((Mole) d);
 }
 
 int main() {
