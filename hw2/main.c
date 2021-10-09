a49b8dd55e262c59510c4f6537351b32b61331c9
Phillip <phillip.bruce@hey.com>
2021-10-08 19:25:27 -0600

    Never gonna give you up

--- hw2/main.c
+++ hw2/main.c
@@ -14,22 +14,16 @@ typedef struct Params {
     Lawn lawn;
 } Params;
 
-static void *produce(void *a) {
-//    Params *p = (Params *) a;
-//    Deq q = (Deq) p->q;
-//    Lawn l = (Lawn) p->lawn;
-//    Data *d = (Data *) mole_new(l, 0, 0);
-//    mtq_tail_put(q, *d);
-//    return 0;
-    void **args = a;
-    Deq q = (Deq)args[0];
-    printf("L E N G T H: %d\n", deq_len(q));
-    Lawn l = (Lawn)args[1];
-    mtq_tail_put(q, mole_new(l, 0, 0));
+static void* produce(void *a) {
+    Params *p = (Params *) a;
+    Deq q = (Deq) p->q;
+    Lawn l = (Lawn) p->lawn;
+    Data *d = (Data *) mole_new(l, 0, 0);
+    mtq_tail_put(q, *d);
     return 0;
 }
 
-static void *consume(void *a) {
+static void* consume(void *a) {
     Params *p = (Params *) a;
     Deq q = (Deq) p->q;
     mole_whack((Mole) mtq_head_get(q));
@@ -51,16 +45,16 @@ int main() {
     // create threads
     pthread_t threads[n * 2];
     for (int i = 0; i < n * 2;) {
-        pthread_create(&threads[i++], NULL, produce, (void *) params);
-        pthread_create(&threads[i++], NULL, consume, (void *) params);
+        pthread_create(&threads[i++], 0, produce, (void *) params);
+        pthread_create(&threads[i++], 0, consume, (void *) params);
     }
     printf("Waiting\n");
     sleep(5);
     printf("Going again\n");
     // join threads
     for (int i = 0; i < n * 2;) {
-        pthread_join(threads[i++], NULL);
-        pthread_join(threads[i++], NULL);
+        pthread_join(threads[i++], 0);
+        pthread_join(threads[i++], 0);
     }
     // free vars
     free(params);
