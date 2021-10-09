ca0bf296dc3947452020b24d57c09bdf4ab62c32
Phillip <phillip.bruce@hey.com>
2021-10-08 21:08:24 -0600

    AAAAAAAAAAAAAAAAAAAAAAAAA

--- hw2/main.c
+++ hw2/main.c
@@ -21,7 +21,6 @@ static void *produce(void *a) {
     Deq q = t.q;
     Lawn l = t.lawn;
     mtq_tail_put(q, mole_new(l, 0, 0));
-    free(p);
     return 0;
 }
 
@@ -31,7 +30,6 @@ static void *consume(void *a) {
     Params t = *p;
     Deq q = t.q;
     mole_whack(mtq_head_get(q));
-    free(p);
     return 0;
 }
 
