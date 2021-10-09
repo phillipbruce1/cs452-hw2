39515adc326e871108af07d4678437f616c9fe44
Phillip <phillip.bruce@hey.com>
2021-10-08 18:08:09 -0600

    Never gonna let you down

--- hw2/main.c
+++ hw2/main.c
@@ -12,11 +12,11 @@
 typedef struct Params {
     Deq q;
     Lawn lawn;
-} *Params;
+} Params;
 
 static void *produce(void *a) {
     void **args = a;
-    Params p = *(Params*) a;
+    Params p = *(Params *) a;
     Deq q = (Deq) p->q;
     Lawn l = (Lawn) p->lawn;
     mtq_tail_put(q, mole_new(l, 0, 0));
@@ -25,21 +25,21 @@ static void *produce(void *a) {
 
 static void *consume(void *a) {
     void **args = a;
-    Params p = *(Params*) a;
+    Params p = *(Params *) a;
     Deq q = (Deq) p->q;
     mole_whack(mtq_head_get(q));
     return 0;
 }
 
 static void create_threads(pthread_t *threads, int n, void *params) {
-    for (int i = 0; i < n * 2; ) {
+    for (int i = 0; i < n * 2;) {
         pthread_create(&threads[i++], 0, produce, params);
         pthread_create(&threads[i++], 0, consume, params);
     }
 }
 
 static void join_threads(pthread_t *threads, int n) {
-    for (int i = 0; i < n * 2; ) {
+    for (int i = 0; i < n * 2;) {
         pthread_join(threads[i++], 0);
         pthread_join(threads[i++], 0);
     }
@@ -52,12 +52,12 @@ int main() {
     Deq q = deq_new();
     Lawn lawn = lawn_new(0, 0);
     // initialize params
-    Params params = (Params) malloc(sizeof(Params));
+    Params *params = (Params *) malloc(sizeof(Params));
     params->q = q;
     params->lawn = lawn;
     // create threads
     pthread_t threads[n * 2];
-    create_threads(threads, n, params);
+    create_threads(threads, n, (void *) params);
     join_threads(threads, n);
     // free vars
     free(params);
